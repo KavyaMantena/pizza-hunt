545d7646b96b0402761e3de9eba51f71cf41cd02
Kavya Mantena <newtechkn@kavyas-air.lan>
2022-06-11 14:51:00 -0500

    created pizza model

---
+++ models/Pizza.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+
+const PizzaSchema = new Schema({
+  pizzaName: {
+    type: String,
+  },
+  createdBy: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  size: {
+    type: String,
+    default: "Large",
+  },
+  toppings: [],
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
+});
+
+const Pizza = model("Pizza", PizzaSchema);
+
+module.exports = Pizza;
